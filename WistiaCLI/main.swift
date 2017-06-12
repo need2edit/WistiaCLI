1df27e9e270444343fb626bde50879390a0503d1
Jake Young <need2edit@gmail.com>
2017-06-12 12:34:15 -0400

    Project setup

---
+++ WistiaCLI/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  WistiaCLI
+//
+//  Created by Jake Young on 6/12/17.
+//  Copyright © 2017 Jake young. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
